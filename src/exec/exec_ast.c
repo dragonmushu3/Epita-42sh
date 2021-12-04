89043871aeaf3dbb5d4698baa805e32b9af0e42b
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-12-04 01:34:31 +0100

    tried to make exec module

--- src/exec/exec_ast.c
+++ src/exec/exec_ast.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-static void execute_in_child(char **args)
+static int execute_in_child(char **args)
 {
     int pid = fork();
     if (pid == 0)
@@ -18,43 +18,68 @@ static void execute_in_child(char **args)
         if (WIFEXITED(status))
         {
             if (WEXITSTATUS(status) == 127)
+            {
                 warnx("%s: command not found", args[0]);
+                return WEXITSTATUS(status);
+            }
+            else
+            {
+                return WEXITSTATUS(status);
+            }
         }
+        else
+            return 1;
     }
+    return 1;
 }
 /*need to check for builtins*/
 
-void exec_ast(struct ast *ast)
+int exec_ast(struct ast *ast)
 {
     if (ast == NULL)
-        return;
+        return 1;
 
     if (ast->type == AST_SIMPLE_COMM)
     {
         /*this could be put in a sub_function called exec_ast_simple_comm*/
         /*check if it's echo or other built-in or it won't work*/
         /*fix me*/
-        execute_in_child(ast->data);
+        return execute_in_child(ast->data);
     }
-    else if (ast->type == AST_COMM)
+    else if (ast->type == AST_LIST)
     {
         /*this could be put in a sub_function called exec_ast_comm */
+        int res = 0;
         if (!ast->children)
         {
-            warn("This command has no simple_commands attached to it! This shouldn't happen...");
+            warn("This list has no simple_commands attached to it! This shouldn't happen...");
         }
         else
         {
             size_t i = 0;
             while (ast->children[i])
             {
-                execute_in_child(ast->children[i]->data);
+                res = exec_ast(ast->children[i]);
                 i++;
             }
         }
+        return res;
+    }
+    else if (ast->type == AST_IF)
+    {
+        if (exec_ast(ast->children[0]))
+        {
+            if (ast->children[2])
+            {
+                exec_ast(ast->children[2]);
+            }
+            return exec_ast(ast->children[1]);
+        }
+        return 1;
     }
     else
     {
         /*other node types when implemented*/
+        return 1;
     }
 }
