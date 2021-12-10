36fc4713ae937ec07bc18879f7c484743df9f897
Mushu Damaschin <mushu.damaschin@epita.fr>
2021-12-10 19:24:15 +0100

    added test

--- tests/print_test.sh
+++ tests/print_test.sh
@@ -22,3 +22,12 @@ else
     echo test 2 failed'!'
     diff -u --color tests/42sh_output tests/sh_output
 fi
+
+./builddir/42sh -c "if true; then echo foo; else echo nope;fi" 1> tests/42sh_output
+sh -c "if true; then echo foo; else echo nope;fi" 1> tests/sh_output
+if cmp -s -- tests/42sh_output tests/sh_output; then
+    echo test 3 passed'!'
+else
+    echo test 3 failed'!'
+    diff -u --color tests/42sh_output tests/sh_output
+fi
